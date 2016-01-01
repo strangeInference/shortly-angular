2bcacb96a3fbad826476d485709f967dc34469f1
strangeInference <tesla.phipps@gmail.com>
2015-12-31 16:16:24 -0800

    Finished addOne and getAll methods

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -2,10 +2,23 @@ angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
   // Your code here
-  var getAll = function(){
-
+  var getAll = function () {
+    return $http({
+      method: 'GET',
+      url: '/api/links',
+    })
+    .then(function (resp) {
+      return resp.data;
+    });
   };
-  var addOne = function(){};
+  var addOne = function (link) {
+    return $http({
+      method: 'POST',
+      url: '/api/links',
+      data: link
+    });
+    
+  };
   return {getAll: getAll,
           addOne: addOne};
 
