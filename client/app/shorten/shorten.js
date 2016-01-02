6094534f8f0862630018b7cece1090175af65831
strangeInference <tesla.phipps@gmail.com>
2016-01-02 12:04:19 -0800

    Updated views to have more information about the links

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,15 +2,19 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
-  // $scope.link = {};
+  $scope.link = {};
+  $scope.mybool = true;
   $scope.addLink = function (link) {
     console.log(link);
-    Links.addOne(link);
-    $scope.link = link;
+    Links.addOne(link)
+    .then(function(data){
+      $scope.link = data;
+    });
   };
-  $scope.submit = function(){
-    if ($scope.text){
-      $scope.addLink(this.text);
+  $scope.submit = function () {
+    if ($scope.text) {
+      $scope.addLink(this.text); 
+      $scope.mybool = false;
     }
-  }
+  };
 });
